729b9b897d62574a4083a9ab90e1ed92b9e6a3bc
richard rekow <richardrekow@gmail.com>
2023-05-21 15:02:20 -0600

    fix again

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,7 +21,7 @@ const Home: NextPage = () => {
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
           {user ? (
-            `hello ${user.data?.user.name?.toString()}`
+            `hello ${user.data?.user.name?.toString() || ""}`
           ) : (
             <button onClick={() => void signIn()}> Sign In</button>
           )}
