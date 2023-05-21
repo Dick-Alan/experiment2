d19f710fd936cbc939edc4bc2a43d314dd1c6d2c
richard rekow <richardrekow@gmail.com>
2023-05-21 14:57:19 -0600

    auth fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,7 +20,7 @@ const Home: NextPage = () => {
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          {user.data?.user.id ? (
+          {user ? (
             `hello ${user.data?.user.name}`
           ) : (
             <button onClick={() => signIn()}> Sign In</button>
